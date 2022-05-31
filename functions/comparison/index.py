7a15ebbd1865cbbc3697221eeb5d2228780e6d4b
TomerGutman1 <106603564+TomerGutman1@users.noreply.github.com>
2022-05-31 17:12:31 +0300

    Update index.py

--- functions/comparison/index.py
+++ functions/comparison/index.py
@@ -2,18 +2,15 @@ from skimage.metrics import structural_similarity as ssim
 import matplotlib.pyplot as plt
 import numpy as np
 import cv2
-
 def mse(imageA, imageB):
     # the 'Mean Squared Error' between the two images is the
     # sum of the squared difference between the two images;
     # NOTE: the two images must have the same dimension
     err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
     err /= float(imageA.shape[0] * imageA.shape[1])
-
     # return the MSE, the lower the error, the more "similar"
     # the two images are
     return err
-
 def compare_images(imageA, imageB):
 	# compute the mean squared error and structural similarity
 	# index for the images
@@ -32,15 +29,3 @@ def compare_images(imageA, imageB):
 	plt.axis("off")
 	# show the images
 	plt.show()
-	
-def img_proc(image):
-    img = cv2.imread('image',0)  # read in grayscale
-    img = cv2.medianBlur(image,3)  #median filtering
-
-    clahe = cv2.createCLAHE(clipLimit=3)
-    final_img = clahe.apply(img)  # Adaptive Histogram Equalization
-
-    bin_Thr = cv2.adaptiveThreshold(final_img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,15,2)
-    bin_Thr=255 - bin_Thr  ## inverting the image B2W for pattern algorithm
-
-    return bin_Thr
